278c2cbad39915a0944da6ae101a7242519be6f3
Aaron Scherer <aequasi@gmail.com>
2012-10-11 16:08:38 -0700

    Adding new configs for daemon

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -3,6 +3,7 @@ namespace Uecode\CommonBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+
 /**
  * @author Aaron Scherer
  * @date 10/8/12
@@ -20,9 +21,9 @@ class Configuration implements ConfigurationInterface
 	 *
 	 * @param Boolean $debug Whether to use the debug mode
 	 */
-	public function  __construct($debug)
+	public function  __construct( $debug )
 	{
-		$this->debug = (Boolean) $debug;
+		$this->debug = (Boolean)$debug;
 	}
 
 	/**
@@ -31,14 +32,13 @@ class Configuration implements ConfigurationInterface
 	public function getConfigTreeBuilder()
 	{
 		$treeBuilder = new TreeBuilder();
-		$rootNode = $treeBuilder->root( 'uecode_common' );
+		$rootNode    = $treeBuilder->root( 'uecode_common' );
 
 		$rootNode
 			->children()
-				->append( $this->addGearmanNode() )
-				->append( $this->addDaemonNode() )
-			->end()
-		;
+			->append( $this->addGearmanNode() )
+			->append( $this->addDaemonNode() )
+			->end();
 
 		return $treeBuilder;
 	}
@@ -49,44 +49,12 @@ class Configuration implements ConfigurationInterface
 	public function addGearmanNode()
 	{
 		$treeBuilder = new TreeBuilder();
-		$rootNode = $treeBuilder->root( 'gearman' );
+		$rootNode    = $treeBuilder->root( 'gearman' );
 
 		$rootNode
 			->children()
-			->scalarNode( 'timeout' )->defaultValue( 10 )->end()
-			->arrayNode( 'connections' )
-			->requiresAtLeastOneElement()
-			->useAttributeAsKey( 'name' )
-			->prototype( 'array' )
-			->children()
-			->scalarNode( 'host' )
-			->defaultValue( '127.0.0.1' )
-			->end()
-			->scalarNode( 'port' )
-			->defaultValue( '4730' )
-			->end()
-			->end()
-			->end()
-			->end()
-			->scalarNode( 'debug' )
-			->defaultValue( 'false' )
-			->end()
-			->end()
-		;
-		return $rootNode;
-	}
-
-	/**
-	 * @return \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition|\Symfony\Component\Config\Definition\Builder\NodeDefinition
-	 */
-	public function addDaemonNode()
-	{
-		$treeBuilder = new TreeBuilder();
-		$rootNode = $treeBuilder->root( 'daemon' );
-
-		$rootNode
-			->children()
-				->arrayNode( 'daemons' )
+				->scalarNode( 'timeout' )->defaultValue( 10 )->end()
+				->arrayNode( 'connections' )
 					->requiresAtLeastOneElement()
 					->useAttributeAsKey( 'name' )
 					->prototype( 'array' )
@@ -100,9 +68,50 @@ class Configuration implements ConfigurationInterface
 						->end()
 					->end()
 				->end()
-				->scalarNode( 'debug' )->defaultValue( 'false' )->end()
+				->scalarNode( 'debug' )
+					->defaultValue( 'false' )
+				->end()
 			->end()
 		;
+		return $rootNode;
+	}
+
+	/**
+	 * @return \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition|\Symfony\Component\Config\Definition\Builder\NodeDefinition
+	 */
+	public function addDaemonNode()
+	{
+		$treeBuilder = new TreeBuilder();
+		$rootNode    = $treeBuilder->root( 'daemon' );
+
+		$rootNode
+			->children()
+				->arrayNode( 'daemons' )
+					->requiresAtLeastOneElement()
+					->useAttributeAsKey( 'name' )
+					->prototype( 'array' )
+						->children()
+							->scalarNode( 'appName' )->defaultValue( 'default' )->end()
+							->scalarNode( 'appDir' )->defaultValue( '%kernel.root_dir%' )->end()
+							->scalarNode( 'appDescription' )->defaultValue( 'Default Description' )->end()
+							->scalarNode( 'logLocation' )->defaultValue( '%kernel.root_dir%' )->end()
+							->scalarNode( 'authorName' )->defaultValue( '' )->end()
+							->scalarNode( 'authorEmail' )->defaultValue( '' )->end()
+							->scalarNode( 'appPidLocation' )->defaultValue( '%kernel.cache_dir%/example/example.pid' )->end()
+							->scalarNode( 'sysMaxExecutionTime' )->defaultValue( '0' )->end()
+							->scalarNode( 'sysMaxInputTime' )->defaultValue( '0' )->end()
+							->scalarNode( 'sysMemoryLimit' )->defaultValue( '1024M' )->end()
+							->scalarNode( 'appUser' )->defaultValue( 'www-data' )->end()
+							->scalarNode( 'appGroup' )->defaultValue( 'www-data' )->end()
+							->scalarNode( 'appRunAsGID' )->defaultValue( '1000' )->end()
+							->scalarNode( 'appRunAsUID' )->defaultValue( '1000' )->end()
+						->end()
+					->end()
+				->end()
+			->scalarNode( 'debug' )->defaultValue( 'false' )->end()
+			->end()
+		;
+
 		return $rootNode;
 	}
 
