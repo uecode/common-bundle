c55d20406d0c8e69ac0af69f2e17418b85905b85
Aaron Scherer <aequasi@gmail.com>
2014-01-14 10:14:44 -0800

    Fixing user service dependencies

--- src/Uecode/Bundle/CommonBundle/Service/UserService.php
+++ src/Uecode/Bundle/CommonBundle/Service/UserService.php
@@ -20,18 +20,14 @@
  */
 namespace Uecode\Bundle\CommonBundle\Service;
 
-use \Symfony\Component\Security\Core\SecurityContext;
-
+use \Symfony\Component\Security\Core\SecurityContextInterface;
 use \Uecode\Bundle\CommonBundle\Model\UserInterface;
-use \Uecode\Bundle\CommonBundle\Traits\DatabaseAwareTrait;
 
 /**
  * User service, for fetching user data
  */
 class UserService extends Service
 {
-
-	use DatabaseAwareTrait;
 
 	/**
 	 * @var UserInterface|null
@@ -42,25 +38,13 @@ class UserService extends Service
 	 * @var SecurityContext
 	 */
 	private $securityContext;
-	
-	/**
-	 * @var string
-	 */
-	private $userEntity;
 
 	/**
-	 * @var string
+	 * @param SecurityContextInterface $securityContext
 	 */
-	private $idProperty;
-
-	/**
-	 * @param string $userEntity
-	 * @param string $idProperty
-	 */
-	public function __construct( $userEntity, $idProperty )
+	public function __construct( SecurityContextInterface $securityContext = null )
 	{
-		$this->userEntity = $userEntity;
-		$this->idProperty = $idProperty;
+		$this->securityContext = $securityContext;
 	}
 
 	/**
@@ -79,60 +63,15 @@ class UserService extends Service
 	 * @return User
 	 */
 	public function getUser()
-	{
-		if ( is_null( $this->user ) ) {
-			$token = $this->getSecurityContext()->getToken();
+    {
+        if (null === $token = $this->securityContext->getToken()) {
+            return null;
+        }
 
-			if ( $token === null ) {
-				$user = 'anon.';
-			} else {
-				$user = $token->getUser();
-			}
+        if (!is_object($user = $token->getUser())) {
+            return null;
+        }
 
-			if ( !is_string( $user ) ) {
-				if( !( $user instanceof UserInterface ) ) {
-					throw new \Exception( sprintf(
-						"Expected user to be of type %s",
-						"\Uecode\Bundle\CommonBundle\Model\UserInterface"
-					) );
-				}
-
-				$this->user = $this->fetchUser( $user->getUserId() );
-			} else {
-				$this->user = $user;
-			}
-		}
-
-		return $this->user;
-	}
-
-	/**
-	 * @param \Symfony\Component\Security\Core\SecurityContext $securityContext
-	 *
-	 * @return UserService
-	 */
-	public function setSecurityContext( SecurityContext $securityContext )
-	{
-		$this->securityContext = $securityContext;
-
-		return $this;
-	}
-
-	/**
-	 * @return \Symfony\Component\Security\Core\SecurityContext
-	 */
-	public function getSecurityContext()
-	{
-		return $this->securityContext;
-	}
-
-	public function fetchUser( $userId )
-	{
-		$repo = $this->getEntityManager()->getRepository( $this->userEntity );
-
-		/** @var $user User */
-		$user = $repo->findOneBy( [ $this->idProperty => $userId ] );
-
-		return $user;
+        return $user;
 	}
 }
